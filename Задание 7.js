11f898005386c89c0c13ed8fd2cf5be779b88b16
Sergei <92303114+Serg7578@users.noreply.github.com>
2023-06-20 09:49:57 +0300

    Update Задание 7.js

--- Задание 7.js
+++ Задание 7.js
@@ -1,11 +1,16 @@
 // Задание 7
-let arr = [1,2,3,4,5,6,0,7,0,8,9,10,'sd',null,28]
-let res=0
-let nul=0
-let str=0
-for (let i=0; i<arr.length; i=i+1){
-    if (arr[i]%2==0 && arr[i]!= 0 && arr[i]!=null) res+=1
-    if (arr[i]===0) nul+=1
-    if (isNaN(arr[i])) str+=1
+
+let arr = [1, 0, 8, 9, 10, 'sd', null, 28]
+let even = 0
+let nul = 0
+let odd = 0
+for (let i = 0; i < arr.length; i = i + 1) {
+    if (typeof arr[i] === "number" && !isNaN(arr[i])) {
+        if (arr[i] !== 0) {
+            if (arr[i] % 2 == 0) even += 1
+            else odd += 1
+        }
+        else nul += 1
+    }
 }
-console.log ('чётных чисел в массиве - '+ res +' , ноль встречается - '+ nul + ' раз(а)' + ', не чисел - '+ str)
+ console.log('чётных чисел в массиве - ' + even + ' , ноль встречается - ' + nul + ' раз(а)' + ', нечётных чисел - ' + odd)
